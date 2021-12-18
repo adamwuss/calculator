6c0f52a748a75da5344697d4212271e7ef49ee05
Adam Wuss <adamwusspl@gmail.com>
2021-12-18 14:47:19 +0100

    add favicon and refactor code

--- js/functions/numberListener.js
+++ js/functions/numberListener.js
@@ -3,21 +3,21 @@ import { btnNumber, input } from '../buttons/buttons.js';
 const numberListener = (starterData, flag) => {
   for (const element of btnNumber) {
     element.addEventListener('click', () => {
-      if (!flag.char && flag.firstNumber) {
-        flag.firstNumber = true;
-        starterData.firstNumber += element.value;
-        input.value = `${starterData.firstNumber} ${starterData.char} ${starterData.secondNumber}`;
-      } else if (!flag.char && !flag.firstNumber) {
+      if (!flag.char && !flag.firstNumber) {
         flag.firstNumber = true;
         starterData.firstNumber = element.value;
         input.value = `${starterData.firstNumber} ${starterData.char} ${starterData.secondNumber}`;
-      } else if (flag.char && flag.secondNumber) {
-        flag.secondNumber = true;
-        starterData.secondNumber += element.value;
+      } else if (!flag.char && flag.firstNumber) {
+        flag.firstNumber = true;
+        starterData.firstNumber += element.value;
         input.value = `${starterData.firstNumber} ${starterData.char} ${starterData.secondNumber}`;
       } else if (flag.char && !flag.secondNumber) {
         flag.secondNumber = true;
         starterData.secondNumber = element.value;
+        input.value = `${starterData.firstNumber} ${starterData.char} ${starterData.secondNumber}`;
+      } else if (flag.char && flag.secondNumber) {
+        flag.secondNumber = true;
+        starterData.secondNumber += element.value;
         input.value = `${starterData.firstNumber} ${starterData.char} ${starterData.secondNumber}`;
       }
     });
